f814aad2b3edeb4b2b4646b355fab267e74fd72d
Taylor Williams <taylor@thewilliams.net>
2018-05-01 22:57:37 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  board = Array.new(9,"   ")
-  counter = 0
-  puts "#{board[counter]}|#{board[counter+1]}|#{board[counter+2]}"
-  puts "-----------"
-  counter += 2
-  puts "#{board[counter]}|#{board[counter+1]}|#{board[counter+2]}"
-  puts "-----------"
-  counter += 2
-  puts "#{board[counter]}|#{board[counter+1]}|#{board[counter+2]}"
+  def display_row
+    empty_board_space = "   "
+    "#{empty_board_space}|#{empty_board_space}|#{empty_board_space}"
+  end
+  def display_row_separation
+    "-----------"
+  end
+  puts display_row
+  puts display_row_separation
+  puts display_row
+  puts display_row_separation
+  puts display_row
 end
